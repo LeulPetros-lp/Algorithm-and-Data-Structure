e87b2311361272d8fc3421e7fb533067b68a00b7
Leul Petros <162853421+LeulPetros-lp@users.noreply.github.com>
2024-04-29 16:01:54 +0300

    Add files via upload

---
+++ Data structure & Algorithm/area.js/area-alg.js
@@ -0,0 +1,7 @@
+const area_alg = (l,w) => {
+    let area = l*w
+
+    return area
+}
+
+module.exports = area_alg
---
+++ Data structure & Algorithm/area.js/area.js
@@ -0,0 +1,4 @@
+const area_alg = require('./area-alg')
+
+let response = area_alg(5,10)
+console.log(response)
